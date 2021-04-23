bb7148f950a0551413db0117589e6e11b10366f0
Denis Novikov <rd3bax@gmail.com>
2021-04-23 14:08:58 +0300

    Добавьте файлы проекта.

---
+++ Tests/AmberCastle.WPF.Styles.WPFTests/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AmberCastle.WPF.Styles.WPFTests
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Tests/AmberCastle.WPF.Styles.WPFTests/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace AmberCastle.WPF.Styles.WPFTests
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
